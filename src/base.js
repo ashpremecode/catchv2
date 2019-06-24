3f976936911d4b7afafb66686fefaf28466490ff
ashpremcode <ashpremecode@gmail.com>
2019-06-24 13:14:54 +1000

    set up OAuth

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDXUCNSpi5u07F76httlCTXAA4mPVQlEHs",
-  authDomain: "catch-of-the-day-wes-bos-2.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-wes-bos-2.firebaseio.com"
+  	apiKey: "AIzaSyBair7Ls8dzAbEJfKZMqg7Q_U8vgtgc73E",
+    authDomain: "catchy-cb843.firebaseapp.com",
+    databaseURL: "https://catchy-cb843.firebaseio.com"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
